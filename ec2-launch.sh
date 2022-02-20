10edbe7a108d25fdbb718bf613a9596b45d6ea88
sai k <psai0073@gmail.com>
2022-02-20 18:26:42 -0500

    ec2 launch template

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+#aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+#
+Temp_ID="lt-033d52657b386e840"
+Temp_ver=3
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${Temp_ID},Version=${Temp_ver}
