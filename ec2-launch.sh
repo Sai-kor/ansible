69870ad9bffa58ffff92011dee3fb8950c7350cb
sai k <psai0073@gmail.com>
2022-02-20 18:33:39 -0500

    tags in launch temp

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -5,4 +5,4 @@
 Temp_ID="lt-033d52657b386e840"
 Temp_ver=3
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${Temp_ID},Version=${Temp_ver} |jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${Temp_ID},Version=${Temp_ver} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" |jq
