3004ff8de5898ce0175bef96e51985d091030ec3
sai k <psai0073@gmail.com>
2022-02-21 16:17:03 -0500

    user input component

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 #aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
-#
+
+if [ -z "$1" ]; then
+  echo -e "\e[1;31mInput is missing\e[0m"
+  exit 1
+fi
+
+COMPONENT=$1
+
 Temp_ID="lt-033d52657b386e840"
 Temp_ver=3
 #check if instance is already there
 
- aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend"|jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+ aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq -0 ]; then
    echo "Instance is already there"
  exit
